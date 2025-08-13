7408ecfe30f0d48b12fe5cb759adb64a428de3b0
JaeYoungJung <hanol98@naver.com>
2025-08-13 11:16:32 +0900

    fix: 도메인 변경에 따라 cors 허용 origin 변경

--- src/main/java/org/jeayoung/template/planetbackend/configuration/CorsConfig.java
+++ src/main/java/org/jeayoung/template/planetbackend/configuration/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
         config.setAllowedOriginPatterns(List.of(
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "https://spring-security-frontend.jaeyoung.store"
+            "https://planet-frontend.jaeyoung.store"
         ));
 //        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of(

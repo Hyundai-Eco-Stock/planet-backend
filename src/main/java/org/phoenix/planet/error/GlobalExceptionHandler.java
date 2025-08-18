f2f7ff308c262a291dfbbe353dbf5c1b8aa08f6a
imyxxnsj <imyxxnsj@gmail.com>
2025-08-18 16:59:05 +0900

    feat: @Valid 유효성 검사 에러 처리 추가

--- src/main/java/org/phoenix/planet/error/GlobalExceptionHandler.java
+++ src/main/java/org/phoenix/planet/error/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package org.phoenix.planet.error;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.phoenix.planet.error.auth.TokenException;
 import org.phoenix.planet.error.order.OrderException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,9 +23,9 @@ public class GlobalExceptionHandler {
         body.put("message", e.getError().getValue());
 
         return ResponseEntity
-            .status(e.getError().getHttpStatus())
-            .header("X-Error-Code", e.getError().name())
-            .body(body);
+                .status(e.getError().getHttpStatus())
+                .header("X-Error-Code", e.getError().name())
+                .body(body);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -33,8 +36,8 @@ public class GlobalExceptionHandler {
         body.put("message", e.getMessage());
 
         return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(body);
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(body);
     }
 
     @ExceptionHandler(Exception.class)
@@ -45,8 +48,8 @@ public class GlobalExceptionHandler {
         body.put("message", e.getMessage());
 
         return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(body);
+                .status(HttpStatus.BAD_REQUEST)
+                .body(body);
     }
 
     /**
@@ -61,6 +64,31 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getError().getHttpStatus())
                 .header("X-Error-Code", e.getError().name())
+                .body(body);
+    }
+
+    /**
+     * 유효성 검사 실패 처리
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException e) {
+        Map<String, Object> body = new HashMap<>();
+        Map<String, String> errors = new HashMap<>();
+
+        if (e.getBindingResult() != null) {
+            e.getBindingResult().getAllErrors().forEach((error) -> {
+                String fieldName = ((FieldError) error).getField();
+                String errorMessage = error.getDefaultMessage();
+                errors.put(fieldName, errorMessage);
+            });
+        }
+
+        body.put("errorCode", e.getClass().getSimpleName());
+        body.put("message", e.getMessage());
+        body.put("errors", errors);
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .body(body);
     }
 

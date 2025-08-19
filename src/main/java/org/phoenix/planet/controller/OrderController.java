f4e44dda6154781b5bc11513b0b737f308d09ae9
imyxxnsj <imyxxnsj@gmail.com>
2025-08-19 11:37:46 +0900

    feat: 주문 생성 API 컨트롤러 추가

---
+++ src/main/java/org/phoenix/planet/controller/OrderController.java
@@ -0,0 +1,34 @@
+package org.phoenix.planet.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.phoenix.planet.annotation.LoginMemberId;
+import org.phoenix.planet.dto.order.request.CreateOrderRequest;
+import org.phoenix.planet.dto.order.response.CreateOrderResponse;
+import org.phoenix.planet.service.order.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<CreateOrderResponse> createOrder(
+            @Valid @RequestBody CreateOrderRequest request,
+            @LoginMemberId Long memberId
+    ) {
+        CreateOrderResponse response = orderService.createOrder(request, memberId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+}

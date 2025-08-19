53754c2e747d43ee25654c1a7da785e8d8ae2f7c
JaeYoungJung <hanol98@naver.com>
2025-08-19 19:02:38 +0900

    fix: firebase key 파일 위치를 resources 외부 (프로젝트 외부)로 이동

--- src/main/java/org/phoenix/planet/configuration/firebase/FirebaseInitializer.java
+++ src/main/java/org/phoenix/planet/configuration/firebase/FirebaseInitializer.java
@@ -4,21 +4,20 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import java.io.InputStream;
+import java.io.FileInputStream;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FirebaseInitializer {
 
     @Value("${fcm.credentials.file}")
-    private Resource firebaseResource;
+    private String firebaseConfigPath;
 
     @PostConstruct
     public void init() {
 
-        try (InputStream serviceAccount = firebaseResource.getInputStream()) {
+        try (FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath)) {
             FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();

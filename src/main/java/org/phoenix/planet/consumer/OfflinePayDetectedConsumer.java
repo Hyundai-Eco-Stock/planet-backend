0685bebdf6240b70a5e7ac9735761ae70a7e065a
JaeYoungJung <hanol98@naver.com>
2025-08-21 18:43:54 +0900

    rename: 클래스 이름 수정

--- src/main/java/org/phoenix/planet/consumer/OfflinePayDetectedConsumer.java
+++ src/main/java/org/phoenix/planet/consumer/OfflinePayDetectedConsumer.java
@@ -25,7 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ReceiptEventConsumer {
+public class OfflinePayDetectedConsumer {
 
     private final ObjectMapper objectMapper;
 

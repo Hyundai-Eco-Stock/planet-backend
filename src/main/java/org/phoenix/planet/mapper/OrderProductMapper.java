f00fc6cc75c23029bbc2b4fe78606abaf1437a43
imyxxnsj <imyxxnsj@gmail.com>
2025-08-26 16:11:51 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/org/phoenix/planet/mapper/OrderProductMapper.java
+++ src/main/java/org/phoenix/planet/mapper/OrderProductMapper.java
@@ -1,10 +1,7 @@
 package org.phoenix.planet.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.phoenix.planet.dto.order.raw.OrderProduct;
-
-import java.util.List;
 
 @Mapper
 public interface OrderProductMapper {

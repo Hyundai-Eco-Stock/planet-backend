ce899e3e946d770a54dd10ef17f8a556efa6fd39
JaeYoungJung <hanol98@naver.com>
2025-08-26 10:46:04 +0900

    feat: 회원가입 시 비밀번호 정규식 검사하도록 함

--- src/main/java/org/phoenix/planet/dto/member/request/SignUpRequest.java
+++ src/main/java/org/phoenix/planet/dto/member/request/SignUpRequest.java
@@ -3,12 +3,18 @@ package org.phoenix.planet.dto.member.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.phoenix.planet.constant.Sex;
 
 public record SignUpRequest(
     @Email @NotBlank String email,
     @NotBlank String name,
-    @NotBlank String password,
+    @NotBlank
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{10,}$",
+        message = "비밀번호는 영문 대소문자와 특수문자를 포함하여 10자 이상이어야 합니다."
+    )
+    String password,
     @NotNull Sex sex,
     @NotBlank String birth, // "YYYY-MM-DD" 형식
     @NotBlank String address,

ef5993565adea254ecadafee3653584c470545b8
junhyeonpark <j0303p@gmail.com>
2025-08-26 16:37:07 +0900

    feat: 당첨된 사람 알림 보내기 위한 각 사용자별 FCM 토큰 조회

--- src/main/java/org/phoenix/planet/service/fcm/FcmService.java
+++ src/main/java/org/phoenix/planet/service/fcm/FcmService.java
@@ -1,17 +1,10 @@
 package org.phoenix.planet.service.fcm;
 
-import com.google.firebase.messaging.BatchResponse;
-import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.MulticastMessage;
-import com.google.firebase.messaging.Notification;
-import com.google.firebase.messaging.WebpushConfig;
-import com.google.firebase.messaging.WebpushNotification;
-
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.phoenix.planet.util.fcm.FcmUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -20,5 +13,10 @@ public class FcmService {
     public void SendEcoStockIssueNotification(List<String> fcmTokens, String body) {
         
         FcmUtil.sendNotificationToMany(fcmTokens, "에코스톡 지급 완료! \uD83C\uDF89", body);
+    }
+
+    public void sendRaffleWinNotification(List<String> fcmTokens, String body) {
+
+        FcmUtil.sendNotificationToMany(fcmTokens, "래플 당첨! \uD83C\uDF89", body);
     }
 }

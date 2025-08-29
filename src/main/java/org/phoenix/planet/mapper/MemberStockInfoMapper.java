9687556401451202cfe1d8a4bc2e388bdbab91f3
imyxxnsj <imyxxnsj@gmail.com>
2025-08-30 07:49:13 +0900

    feat: 에코스톡 발급용 Mapper 및 XML 쿼리 구현

--- src/main/java/org/phoenix/planet/mapper/MemberStockInfoMapper.java
+++ src/main/java/org/phoenix/planet/mapper/MemberStockInfoMapper.java
@@ -7,4 +7,18 @@ import org.phoenix.planet.dto.eco_stock.raw.MemberStockInfo;
 @Mapper
 public interface MemberStockInfoMapper {
     MemberStockInfo findPersonalStockInfoById(@Param("memberId") Long memberId, @Param("ecoStockId") Long ecoStockId);
+
+    void insertMemberStockInfo(
+        @Param("memberId") Long memberId,
+        @Param("ecoStockId") Long ecoStockId,
+        @Param("quantity") Integer quantity,
+        @Param("amount") Integer amount
+    );
+
+    void updateMemberStockInfo(
+        @Param("memberStockInfoId") Long memberStockInfoId,
+        @Param("newQuantity") Integer newQuantity,
+        @Param("newAmount") Long newAmount
+    );
+
 }

b1fa92f2b90abc76957239641e2cb58a74e9dcc6
junhyeonpark <j0303p@gmail.com>
2025-08-29 11:40:10 +0900

    refactor: fcm 전송 서비스 생성 및 적용

--- src/main/java/org/phoenix/planet/scheduler/RaffleWinningScheduler.java
+++ src/main/java/org/phoenix/planet/scheduler/RaffleWinningScheduler.java
@@ -2,11 +2,14 @@ package org.phoenix.planet.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.phoenix.planet.dto.raffle.raw.WinnerInfo;
+import org.phoenix.planet.service.fcm.NotificationService;
 import org.phoenix.planet.service.raffle.RaffleService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -14,8 +17,9 @@ import java.time.LocalDate;
 public class RaffleWinningScheduler {
 
     private final RaffleService raffleService;
+    private final NotificationService notificationService;
 
-    @Scheduled(cron = "0 0 0 * * *") //
+    @Scheduled(cron = "0 0 4 * * *") //
 //    @Scheduled(cron = "0/30 * * * * *") //테스트용
 //    @DistributedScheduled(lockKey = "raffle:winning:update:lock")
     public void raffleWinningProcess() {
@@ -23,7 +27,10 @@ public class RaffleWinningScheduler {
         LocalDate yesterday = LocalDate.now().minusDays(1);
         log.info("RaffleWinningScheduler");
 
-        raffleService.raffleWinningProcess(yesterday);
+        List<WinnerInfo> winners = raffleService.raffleWinningProcess(yesterday);
+
+        // FCM 처리
+        notificationService.sendRaffleWinNotifications(winners);
 
         log.info("Raffle Winning Process finished");
     }

452f28eeb2b7ff90992f188bfff21c6af46a8b6f
junhyeonpark <j0303p@gmail.com>
2025-08-29 09:28:59 +0900

    feat: 사용자의 에코스톡 보유량 가격 조회 (래플에서 보유량 기반으로 가능여부 판단에 필요 + 이후 마이페이지에서 조회에 사용)

--- src/main/java/org/phoenix/planet/controller/MemberStockInfoController.java
+++ src/main/java/org/phoenix/planet/controller/MemberStockInfoController.java
@@ -5,10 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.phoenix.planet.annotation.LoginMemberId;
 import org.phoenix.planet.dto.eco_stock.raw.MemberStockInfo;
 import org.phoenix.planet.dto.eco_stock.request.SellStockRequest;
+import org.phoenix.planet.dto.eco_stock_info.response.MemberStockInfoWithDetail;
 import org.phoenix.planet.service.eco_stock.EcoStockService;
 import org.phoenix.planet.service.eco_stock.MemberStockInfoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -39,5 +42,18 @@ public class MemberStockInfoController {
         log.info("get user info:{}", memberStockInfo);
 
         return ResponseEntity.ok().body(memberStockInfo);
+    }
+
+    @GetMapping("/summary/all")
+    public ResponseEntity<?> getUserInfoAll(@LoginMemberId Long loginMemberId) {
+
+        log.info("get user info: {}", loginMemberId);
+
+        List<MemberStockInfoWithDetail> memberStockInfoWithDetails =
+                memberStockInfoService.findAllPersonalStockInfoByMemberId(loginMemberId);
+
+        log.info("get user info:{}", memberStockInfoWithDetails);
+
+        return ResponseEntity.ok().body(memberStockInfoWithDetails);
     }
 }

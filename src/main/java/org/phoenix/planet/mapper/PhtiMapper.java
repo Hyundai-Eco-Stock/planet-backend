eaa63545b5a193caf1b869230cd1b3df6cc56926
JaeYoungJung <hanol98@naver.com>
2025-09-01 12:17:30 +0900

    fix: 오타 수정

--- src/main/java/org/phoenix/planet/mapper/PhtiMapper.java
+++ src/main/java/org/phoenix/planet/mapper/PhtiMapper.java
@@ -5,12 +5,11 @@ import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.phoenix.planet.dto.phti.raw.Phti;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value.Str;
 
 @Mapper
 public interface PhtiMapper {
 
-    Optional<Phti> selectByName(@Param("phtiName") Str phtiName);
+    Optional<Phti> selectByName(@Param("phtiName") String phtiName);
 
     List<Phti> selectAll();
 

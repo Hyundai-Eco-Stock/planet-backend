adc8357779d89a0ef3343b4b58bd49551ff2e35c
JaeYoungJung <hanol98@naver.com>
2025-09-02 15:38:29 +0900

    add: 모든 비밀번호 입력 시 유효성 검사 어노테이션 추가

--- src/main/java/org/phoenix/planet/dto/member/request/ProfileUpdateRequest.java
+++ src/main/java/org/phoenix/planet/dto/member/request/ProfileUpdateRequest.java
@@ -2,6 +2,7 @@ package org.phoenix.planet.dto.member.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.phoenix.planet.constant.Sex;
 
 public record ProfileUpdateRequest(
@@ -10,6 +11,10 @@ public record ProfileUpdateRequest(
     @NotBlank String birth,
     @NotBlank String address,
     @NotBlank String detailAddress,
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{10,}$",
+        message = "비밀번호는 대문자, 소문자, 특수문자를 포함하고 10자 이상이어야 합니다."
+    )
     @NotBlank String oldPassword
 ) {
 
--- src/main/java/org/phoenix/planet/dto/member/request/SignUpRequest.java
+++ src/main/java/org/phoenix/planet/dto/member/request/SignUpRequest.java
@@ -6,21 +6,17 @@ import jakarta.validation.constraints.Pattern;
 import org.phoenix.planet.constant.Sex;
 
 public record SignUpRequest(
-//    @Email @NotBlank String email,
-//    @NotBlank String name,
-    @NotBlank
     @Pattern(
         regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).{10,}$",
-        message = "비밀번호는 영문 대소문자와 특수문자를 포함하여 10자 이상이어야 합니다."
+        message = "비밀번호는 대문자, 소문자, 특수문자를 포함하고 10자 이상이어야 합니다."
     )
-    String password,
+    @NotBlank String password,
     @NotNull Sex sex,
-    @NotBlank
     @Pattern(
         regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$",
         message = "생년월일은 YYYY-MM-DD 형식이어야 합니다."
     )
-    String birth,
+    @NotBlank String birth,
     @NotBlank String address,
     @NotBlank String detailAddress
 ) {

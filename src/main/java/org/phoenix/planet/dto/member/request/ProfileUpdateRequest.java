67a1ae6162735d7304e67478f44c90232cc67d32
JaeYoungJung <hanol98@naver.com>
2025-09-02 15:36:50 +0900

    add: 프로필 업데이트할 때 예전 비밀번호도 요청으로 받음

--- src/main/java/org/phoenix/planet/dto/member/request/ProfileUpdateRequest.java
+++ src/main/java/org/phoenix/planet/dto/member/request/ProfileUpdateRequest.java
@@ -5,12 +5,12 @@ import jakarta.validation.constraints.NotNull;
 import org.phoenix.planet.constant.Sex;
 
 public record ProfileUpdateRequest(
-//    @NotBlank String email,
-//    @NotBlank String name,
+
     @NotNull Sex sex,
     @NotBlank String birth,
     @NotBlank String address,
-    @NotBlank String detailAddress
+    @NotBlank String detailAddress,
+    @NotBlank String oldPassword
 ) {
 
 }

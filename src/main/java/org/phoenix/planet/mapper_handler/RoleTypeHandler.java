2840b568ce912904116e0b631e9e68a235f11ddc
JaeYoungJung <hanol98@naver.com>
2025-09-07 14:11:15 +0900

    fix: role type handler를 통해 Role 직접 맵핑

---
+++ src/main/java/org/phoenix/planet/mapper_handler/RoleTypeHandler.java
@@ -0,0 +1,14 @@
+package org.phoenix.planet.mapper_handler;
+
+import org.apache.ibatis.type.EnumTypeHandler;
+import org.apache.ibatis.type.MappedTypes;
+import org.phoenix.planet.constant.member.Role;
+
+@MappedTypes(Role.class)
+public class RoleTypeHandler extends EnumTypeHandler<Role> {
+
+    public RoleTypeHandler() {
+
+        super(Role.class);
+    }
+}

dab12eea6d9582b2687ceefcadbc1869388bd854
junhyeonpark <j0303p@gmail.com>
2025-09-09 02:59:40 +0900

    feat: 스톡 판매 프로시저 및 벌크 insert

--- src/main/java/org/phoenix/planet/service/eco_stock/EcoStockService.java
+++ src/main/java/org/phoenix/planet/service/eco_stock/EcoStockService.java
@@ -3,9 +3,14 @@ package org.phoenix.planet.service.eco_stock;
 import java.time.LocalDateTime;
 import org.phoenix.planet.dto.eco_stock.raw.EcoStock;
 import org.phoenix.planet.dto.eco_stock.raw.EcoStockUpdatePriceRecord;
+import org.phoenix.planet.dto.eco_stock.raw.EcoStockWithLastPrice;
+import org.phoenix.planet.dto.eco_stock.raw.MemberStockInfo;
+import org.phoenix.planet.dto.eco_stock.raw.PointResult;
 import org.phoenix.planet.dto.eco_stock.request.SellStockRequest;
 
 import java.util.List;
+import org.phoenix.planet.dto.eco_stock.response.UnifiedUpdateResult;
+import org.phoenix.planet.dto.eco_stock_info.response.SellResponse;
 
 public interface EcoStockService {
 
@@ -17,5 +22,10 @@ public interface EcoStockService {
 
     void updateQuantityById(Long stockId, int updateQuantity);
 
-    void sellStock(Long memberId, SellStockRequest sellStockRequest);
+    SellResponse sellStock(Long memberId, SellStockRequest sellStockRequest);
+
+    PointResult processUserTransactionAndPoint(Long loginMemberId, SellStockRequest request,
+        UnifiedUpdateResult result, MemberStockInfo memberStockInfo);
+
+    List<EcoStockWithLastPrice> findAllWithLastPrice();
 }
--- src/main/java/org/phoenix/planet/service/eco_stock/EcoStockServiceImpl.java
+++ src/main/java/org/phoenix/planet/service/eco_stock/EcoStockServiceImpl.java
@@ -1,19 +1,24 @@
 package org.phoenix.planet.service.eco_stock;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.phoenix.planet.constant.EcoStockError;
-import org.phoenix.planet.constant.SellStockErrorCode;
-import org.phoenix.planet.dto.eco_stock.raw.EcoStock;
-import org.phoenix.planet.dto.eco_stock.raw.EcoStockUpdatePriceRecord;
-import org.phoenix.planet.dto.eco_stock.request.SellStockRequest;
-import org.phoenix.planet.error.ecoStock.EcoStockException;
-import org.phoenix.planet.mapper.EcoStockMapper;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.phoenix.planet.constant.RaffleError;
+import org.phoenix.planet.dto.eco_stock.raw.EcoStock;
+import org.phoenix.planet.dto.eco_stock.raw.EcoStockUpdatePriceRecord;
+import org.phoenix.planet.dto.eco_stock.raw.EcoStockWithLastPrice;
+import org.phoenix.planet.dto.eco_stock.raw.MemberStockInfo;
+import org.phoenix.planet.dto.eco_stock.raw.PointResult;
+import org.phoenix.planet.dto.eco_stock.request.SellStockRequest;
+import org.phoenix.planet.dto.eco_stock.response.UnifiedUpdateResult;
+import org.phoenix.planet.dto.eco_stock_info.response.SellResponse;
+import org.phoenix.planet.error.raffle.RaffleException;
+import org.phoenix.planet.mapper.EcoStockMapper;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
@@ -21,19 +26,24 @@ import java.util.Optional;
 public class EcoStockServiceImpl implements EcoStockService {
 
     private final EcoStockMapper ecoStockMapper;
+    private final MemberStockInfoService memberStockInfoService;
+    private final StockTradeProcessor stockTradeProcessor;
 
     @Override
     public EcoStock searchById(long ecoStockId) {
 
         return ecoStockMapper.selectById(ecoStockId);
     }
+
     @Override
     public List<EcoStock> findAll() {
+
         return ecoStockMapper.findAll();
     }
 
     @Override
     public void updateQuantityById(Long stockId, int updateQuantity) {
+
         ecoStockMapper.updateQuantityById(stockId, updateQuantity);
     }
 
@@ -44,27 +54,81 @@ public class EcoStockServiceImpl implements EcoStockService {
     }
 
     @Override
-    public void sellStock(Long memberId, SellStockRequest sellStockRequest) {
+    public SellResponse sellStock(Long memberId, SellStockRequest request) {
 
-        ecoStockMapper.callSellStockProcedure(memberId, sellStockRequest);
-        // 프로시저 실행 후, DTO에 채워진 OUT 파라미터 값을 확인
-        Integer successCode = sellStockRequest.getPSuccess();
+        // 1. 사용자 주식 보유량 체크
+        MemberStockInfo memberStockInfo = memberStockInfoService.validateUserStock(memberId,
+            request);
 
-        validateSuccessCode(successCode);
+        UnifiedUpdateResult result = stockTradeProcessor.executeSellTradeAndBroadcast(
+            request.getEcoStockId(), request.getSellCount());
+        //프로시저로 빼기
+        PointResult pointResult = processUserTransactionAndPoint(memberId, request,
+            result, memberStockInfo);
 
-        log.info("판매 성공: {}", sellStockRequest.getPMessage());
+        // 반환값 빌드
+        SellResponse sellResponse = SellResponse.builder()
+            .executedPrice(result.getExecutedPrice())
+            .currentTotalQuantity(Optional.ofNullable(pointResult)
+                .map(PointResult::getCurrentTotalQuantity)
+                .orElse(0))
+            .currentTotalAmount(Optional.ofNullable(pointResult)
+                .map(PointResult::getCurrentTotalAmount)
+                .map(this::roundToTwoDecimals)
+                .orElse(0.0))
+            .memberPoint(Optional.ofNullable(pointResult)
+                .map(PointResult::getMemberPoint)
+                .map(this::roundToTwoDecimals)
+                .orElse(0.0))
+            .build();
+
+        log.info("sell stock result:{}", sellResponse);
+
+        return sellResponse;
     }
 
-    private void validateSuccessCode(Integer successCode) {
+    // 유틸 메서드 생성
+    private double roundToTwoDecimals(double value) {
+        return BigDecimal.valueOf(value)
+            .setScale(2, RoundingMode.HALF_UP)
+            .doubleValue();
+    }
 
-        if (successCode == null || successCode != 1) {
 
-            EcoStockError error = Optional.ofNullable(successCode)
-                    .filter(code -> code < 0)
-                    .map(SellStockErrorCode::getEcoStockError)
-                    .orElse(EcoStockError.INTERNAL_SERVER_ERROR);
+    @Override
+    public PointResult processUserTransactionAndPoint(Long loginMemberId, SellStockRequest request,
+        UnifiedUpdateResult result, MemberStockInfo memberStockInfo) {
 
-            throw new EcoStockException(error);
+        PointResult pointResult = new PointResult();
+
+        ecoStockMapper.callSellStockUserTransaction(loginMemberId, result, request.getSellCount(),
+            memberStockInfo, pointResult);
+
+        log.info("{}", result);
+
+        // 성공 여부 체크
+        if (pointResult.getSuccess() == null) {
+            throw new RaffleException(RaffleError.PROCEDURE_ERROR);
         }
+
+        switch (pointResult.getSuccess()) {
+            case 1: // 성공
+                log.info("✅ 판매 성공: {}", pointResult.getMessage());
+                break;
+            case -3: // 보유 주식 없음
+                throw new RaffleException(RaffleError.INSUFFICIENT_STOCK);
+            case -99: // 프로시저 내부 예외
+                throw new RaffleException(RaffleError.PROCEDURE_ERROR);
+            default: // 그 외 알 수 없는 코드
+                log.error("{}): {}", pointResult.getSuccess(), pointResult.getMessage());
+                throw new RaffleException(RaffleError.RAFFLE_SYSTEM_ERROR);
+        }
+        return pointResult;
+    }
+
+    @Override
+    public List<EcoStockWithLastPrice> findAllWithLastPrice() {
+
+        return ecoStockMapper.findAllWithLastPrice();
     }
 }

7b26506bd9114cdad4bdfdac2bc437f73f4affef
junhyeonpark <j0303p@gmail.com>
2025-09-09 02:59:02 +0900

    feat: 가격 모두 double로 변환 처리

--- src/main/java/org/phoenix/planet/dto/eco_stock/request/SellStockRequest.java
+++ src/main/java/org/phoenix/planet/dto/eco_stock/request/SellStockRequest.java
@@ -1,15 +1,18 @@
 package org.phoenix.planet.dto.eco_stock.request;
 
+import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class SellStockRequest {
     // IN 파라미터
     private Long ecoStockId;
-    private Long sellPrice;
+    private Double sellPrice;
     private Integer sellCount;
 
     // OUT 파라미터를 받을 필드 (프로시저의 OUT 파라미터명과 일치시킬 필요는 없음)
     private Integer pSuccess; // NUMERIC -> Integer 또는 int
     private String pMessage;  // VARCHAR -> String
+    private Double newPrice;
+    private LocalDateTime transactionTime;
 }

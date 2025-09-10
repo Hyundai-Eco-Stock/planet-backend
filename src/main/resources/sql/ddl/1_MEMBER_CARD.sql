41f6e8dc50b41d71f4bffc4bab3c360a8109637c
JaeYoungJung <hanol98@naver.com>
2025-09-11 00:48:38 +0900

    fix: 멤버 카드 테이블에서 카드 번호 컬럼 유니크 속성 추가

--- src/main/resources/sql/ddl/1_MEMBER_CARD.sql
+++ src/main/resources/sql/ddl/1_MEMBER_CARD.sql
@@ -4,7 +4,7 @@ create table MEMBER_CARD
     MEMBER_CARD_ID NUMBER               not null primary key,
     MEMBER_ID      NUMBER
         constraint FK_MEMBER_CARD_MEMBER references MEMBER,
-    CARD_NUMBER    VARCHAR2(255),
+    CARD_NUMBER    VARCHAR2(255) UNIQUE,
     CREATED_AT     DATE default SYSDATE not null,
     UPDATED_AT     DATE
 );
